433fae6a46b39b04f1dcf3ffc7d6b87c9699b382
David Clutter <cluttered.code@gmail.com>
2025-01-13 22:46:21 -0800

    implement multipliers

--- example/main.gd
+++ example/main.gd
@@ -7,46 +7,57 @@ func _ready() -> void:
 	%Killable.pressed.connect(_on_killable_pressed.bind(%Killable))
 	%Revivable.pressed.connect(_on_revivable_pressed.bind(%Revivable))
 	
+	%DamageMultiplier.value_changed.connect(_on_damage_multiplier_changed)
 	%Damage.pressed.connect(_on_damage_pressed)
+	
+	%HealMultiplier.value_changed.connect(_on_heal_multiplier_changed)
 	%Heal.pressed.connect(_on_heal_pressed)
 	
 	$Player.health.died.connect(_on_player_died)
 	$Player.health.revived.connect(_on_player_revived)
 
 
-func _on_damageable_pressed(button: CheckButton):
+func _on_damageable_pressed(button: CheckButton) -> void:
 	$Player.health.damageable = button.button_pressed
 
 
-func _on_healable_pressed(button: CheckButton):
+func _on_healable_pressed(button: CheckButton) -> void:
 	$Player.health.healable = button.button_pressed
 
 
-func _on_killable_pressed(button: CheckButton):
+func _on_killable_pressed(button: CheckButton) -> void:
 	$Player.health.killable = button.button_pressed
 
 
-func _on_revivable_pressed(button: CheckButton):
+func _on_revivable_pressed(button: CheckButton) -> void:
 	$Player.health.revivable = button.button_pressed
 
 
-func _on_damage_pressed():
+func _on_damage_multiplier_changed(value: float) -> void:
+	$Player.hurt_box.damage_multiplier = value
+
+
+func _on_damage_pressed() -> void:
 	$HitBox2D.monitoring = false
 	$HitBox2D.action = HitBox2D.Action.DAMAGE
-	$HitBox2D.amount = 10
+	$HitBox2D.amount = %DamageAmount.value
 	$HitBox2D.monitoring = true
 
 
-func _on_heal_pressed():
+func _on_heal_multiplier_changed(value: float) -> void:
+	$Player.hurt_box.heal_multiplier = value
+
+
+func _on_heal_pressed() -> void:
 	$HitBox2D.monitoring = false
 	$HitBox2D.action = HitBox2D.Action.HEAL
-	$HitBox2D.amount = 10
+	$HitBox2D.amount = %HealAmount.value
 	$HitBox2D.monitoring = true
 
 
-func _on_player_died(_player: Node):
+func _on_player_died(_player: Node) -> void:
 	%Status.text = "Dead"
 
 
-func _on_player_revived(_player: Node):
+func _on_player_revived(_player: Node) -> void:
 	%Status.text = "Alive"
--- example/player/player.gd
+++ example/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var health: Health = $Health
+@onready var hurt_box: HurtBox2D = $HurtBox2D
 @onready var health_bar: ProgressBar = $HealthBar
 
 
