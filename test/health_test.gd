c2f8e05bb2d671873724afc806fcf76338a4965e
David Clutter <cluttered.code@gmail.com>
2025-02-12 23:52:59 -0800

    fully tested

--- test/health_test.gd
+++ test/health_test.gd
@@ -1,11 +1,6 @@
-# GdUnit generated TestSuite
-class_name HealthTest
-extends GdUnitTestSuite
+class_name HealthTest extends GdUnitTestSuite
 @warning_ignore('unused_parameter')
 @warning_ignore('return_value_discarded')
-
-# TestSuite generated from
-const __source = 'res://addons/health_hitbox_hurtbox/health/health.gd'
 
 var health: Health
 var signals: Object
---
+++ test/hurt_box_2d_test.gd
@@ -0,0 +1,85 @@
+class_name HurtBox2DTest extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+var mock_health: Health
+var hurt_box: HurtBox2D
+
+func before_test() -> void:
+	mock_health = auto_free(mock(Health))
+	hurt_box = auto_free(HurtBox2D.new())
+	hurt_box.health = mock_health
+	add_child(hurt_box)
+
+
+func test_damage() -> void:
+	hurt_box.damage(10)
+	
+	verify(mock_health, 1).damage(10)
+
+
+func test_damage_modifier() -> void:
+	hurt_box.damage_multiplier = 1.5
+	hurt_box.damage(10)
+	
+	verify(mock_health, 1).damage(15)
+
+
+func test_damage_no_health() -> void:
+	hurt_box.health = null
+	
+	await assert_error(func(): hurt_box.damage(10))\
+			.is_push_error("%s is missing a 'Health' component" % hurt_box)
+	
+	verify_no_interactions(mock_health)
+
+
+func test_heal() -> void:
+	hurt_box.heal(10)
+	
+	verify(mock_health, 1).heal(10)
+
+
+func test_heal_modifier() -> void:
+	hurt_box.heal_multiplier = 2.4
+	hurt_box.heal(10)
+	
+	verify(mock_health, 1).heal(24)
+
+
+func test_heal_no_health() -> void:
+	hurt_box.health = null
+	
+	await assert_error(func(): hurt_box.heal(10))\
+			.is_push_error("%s is missing a 'Health' component" % hurt_box)
+	
+	verify_no_interactions(mock_health)
+
+
+func test_on_entered_damage() -> void:
+	var hit_box: HitBox2D = auto_free(HitBox2D.new())
+	hit_box.action = HitBox2D.Action.DAMAGE
+	hit_box.amount = 10
+	
+	hurt_box._on_area_entered(hit_box)
+	
+	verify(mock_health, 1).damage(10)
+
+
+func test_on_entered_heal() -> void:
+	var hit_box: HitBox2D = auto_free(HitBox2D.new())
+	hit_box.action = HitBox2D.Action.HEAL
+	hit_box.amount = 10
+	
+	hurt_box._on_area_entered(hit_box)
+	
+	verify(mock_health, 1).heal(10)
+
+
+func test_on_entered_not_hit_box_2d() -> void:
+	var area = auto_free(Area2D.new())
+	
+	hurt_box._on_area_entered(area)
+	
+	verify(mock_health, 0).damage(any_int())
+	verify(mock_health, 0).heal(any_int())
