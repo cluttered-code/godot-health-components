155e19fdd729b49ff453e5137b17bdade7306e31
David Clutter <cluttered.code@gmail.com>
2025-02-19 15:25:26 -0800

    add and remove custom types that are present

--- addons/health_hitbox_hurtbox/plugin.gd
+++ addons/health_hitbox_hurtbox/plugin.gd
@@ -1,43 +1,71 @@
 @tool
 extends EditorPlugin
 
+const addon_path = "res://addons/health_hitbox_hurtbox/%s"
+
+var hit_box_2d_added: bool
+var hit_scan_2d_added: bool
+var hurt_box_2d_added: bool
+
+var hit_box_3d_added: bool
+var hit_scan_3d_added: bool
+var hurt_box_3d_added: bool
+
 func _enter_tree() -> void:
 	add_custom_type("Health", "Node",
 		preload("health/health.gd"),
 		preload("health/health.svg"))
 	
 	# Load 2D components
-	add_custom_type("HitBox2D", "Area2D",
-		preload("2d/hit_box_2d/hit_box_2d.gd"),
-		preload("2d/hit_box_2d/hit_box_2d.svg"))
-	add_custom_type("HitScan2D", "RayCast2D",
-		preload("2d/hit_scan_2d/hit_scan_2d.gd"),
-		preload("2d/hit_scan_2d/hit_scan_2d.svg"))
-	add_custom_type("HurtBox2D", "Area2D",
-		preload("2d/hurt_box_2d/hurt_box_2d.gd"),
-		preload("2d/hurt_box_2d/hurt_box_2d.svg"))
+	if FileAccess.file_exists(addon_path % "2d/hit_box_2d/hit_box_2d.gd"):
+		hit_box_2d_added = true
+		add_custom_type("HitBox2D", "Area2D",
+			preload("2d/hit_box_2d/hit_box_2d.gd"),
+			preload("2d/hit_box_2d/hit_box_2d.svg"))
+	if FileAccess.file_exists(addon_path % "2d/hit_scan_2d/hit_scan_2d.gd"):
+		hit_scan_2d_added = true
+		add_custom_type("HitScan2D", "RayCast2D",
+			preload("2d/hit_scan_2d/hit_scan_2d.gd"),
+			preload("2d/hit_scan_2d/hit_scan_2d.svg"))
+	if FileAccess.file_exists(addon_path % "2d/hurt_box_2d/hurt_box_2d.gd"):
+		hurt_box_2d_added = true
+		add_custom_type("HurtBox2D", "Area2D",
+			preload("2d/hurt_box_2d/hurt_box_2d.gd"),
+			preload("2d/hurt_box_2d/hurt_box_2d.svg"))
 	
 	# Load 3D components
-	add_custom_type("HitBox3D", "Area3D",
-		preload("3d/hit_box_3d/hit_box_3d.gd"),
-		preload("3d/hit_box_3d/hit_box_3d.svg"))
-	add_custom_type("HitScan3D", "RayCast3D",
-		preload("3d/hit_scan_3d/hit_scan_3d.gd"),
-		preload("3d/hit_scan_3d/hit_scan_3d.svg"))
-	add_custom_type("HurtBox3D", "Area3D",
-		preload("3d/hurt_box_3d/hurt_box_3d.gd"),
-		preload("3d/hurt_box_3d/hurt_box_3d.svg"))
+	if FileAccess.file_exists(addon_path % "3d/hit_box_3d/hit_box_3d.gd"):
+		hit_box_3d_added = true
+		add_custom_type("HitBox3D", "Area3D",
+			preload("3d/hit_box_3d/hit_box_3d.gd"),
+			preload("3d/hit_box_3d/hit_box_3d.svg"))
+	if FileAccess.file_exists(addon_path % "3d/hit_scan_3d/hit_scan_3d.gd"):
+		hit_scan_3d_added = true
+		add_custom_type("HitScan3D", "RayCast3D",
+			preload("3d/hit_scan_3d/hit_scan_3d.gd"),
+			preload("3d/hit_scan_3d/hit_scan_3d.svg"))
+	if FileAccess.file_exists(addon_path % "3d/hurt_box_3d/hurt_box_3d.gd"):
+		hurt_box_3d_added = true
+		add_custom_type("HurtBox3D", "Area3D",
+			preload("3d/hurt_box_3d/hurt_box_3d.gd"),
+			preload("3d/hurt_box_3d/hurt_box_3d.svg"))
 
 
 func _exit_tree() -> void:
 	remove_custom_type("Health")
 	
 	# Remove 2D components
-	remove_custom_type("HitBox2D")
-	remove_custom_type("HitScan2D")
-	remove_custom_type("HurtBox2D")
+	if hit_box_2d_added:
+		remove_custom_type("HitBox2D")
+	if hit_scan_2d_added:
+		remove_custom_type("HitScan2D")
+	if hurt_box_2d_added:
+		remove_custom_type("HurtBox2D")
 	
 	# Remove 3D components
-	remove_custom_type("HitBox3D")
-	remove_custom_type("HitScan3D")
-	remove_custom_type("HurtBox3D")
+	if hit_box_3d_added:
+		remove_custom_type("HitBox3D")
+	if hit_scan_3d_added:
+		remove_custom_type("HitScan3D")
+	if hurt_box_3d_added:
+		remove_custom_type("HurtBox3D")
