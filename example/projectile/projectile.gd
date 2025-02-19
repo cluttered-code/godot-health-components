dfc27120f795fc1f7fd6eec332ddfee778ad8995
David Clutter <cluttered.code@gmail.com>
2025-02-19 11:27:14 -0800

    compose projectile

--- example/projectile/projectile.gd
+++ example/projectile/projectile.gd
@@ -1,11 +1,12 @@
-class_name Projectile extends HitBox2D
+class_name Projectile extends Node2D
 
 const SPEED = 600
 
+@onready var hit_box: HitBox2D = $HitBox2D
+
+
 func _ready() -> void:
-	super()
-	
-	hurt_box_entered.connect(_on_hurt_box_entered)
+	hit_box.hurt_box_entered.connect(_on_hurt_box_entered)
 
 
 func _physics_process(delta: float) -> void:
@@ -13,5 +14,5 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_hurt_box_entered(_hurt_box: HurtBox2D) -> void:
-	ignore_collisions = true
+	hit_box.ignore_collisions = true
 	queue_free()
