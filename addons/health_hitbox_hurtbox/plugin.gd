117fb9f62b49e2e59131d3129cad0e5e337eecd4
David Clutter <cluttered.code@gmail.com>
2025-02-19 15:55:32 -0800

    use load for components instead of preload

--- addons/health_hitbox_hurtbox/plugin.gd
+++ addons/health_hitbox_hurtbox/plugin.gd
@@ -20,35 +20,35 @@ func _enter_tree() -> void:
 	if FileAccess.file_exists(addon_path % "2d/hit_box_2d/hit_box_2d.gd"):
 		hit_box_2d_added = true
 		add_custom_type("HitBox2D", "Area2D",
-			preload("2d/hit_box_2d/hit_box_2d.gd"),
-			preload("2d/hit_box_2d/hit_box_2d.svg"))
+			load(addon_path % "2d/hit_box_2d/hit_box_2d.gd"),
+			load(addon_path % "2d/hit_box_2d/hit_box_2d.svg"))
 	if FileAccess.file_exists(addon_path % "2d/hit_scan_2d/hit_scan_2d.gd"):
 		hit_scan_2d_added = true
 		add_custom_type("HitScan2D", "RayCast2D",
-			preload("2d/hit_scan_2d/hit_scan_2d.gd"),
-			preload("2d/hit_scan_2d/hit_scan_2d.svg"))
+			load(addon_path % "2d/hit_scan_2d/hit_scan_2d.gd"),
+			load(addon_path % "2d/hit_scan_2d/hit_scan_2d.svg"))
 	if FileAccess.file_exists(addon_path % "2d/hurt_box_2d/hurt_box_2d.gd"):
 		hurt_box_2d_added = true
 		add_custom_type("HurtBox2D", "Area2D",
-			preload("2d/hurt_box_2d/hurt_box_2d.gd"),
-			preload("2d/hurt_box_2d/hurt_box_2d.svg"))
+			load(addon_path % "2d/hurt_box_2d/hurt_box_2d.gd"),
+			load(addon_path % "2d/hurt_box_2d/hurt_box_2d.svg"))
 	
 	# Load 3D components
 	if FileAccess.file_exists(addon_path % "3d/hit_box_3d/hit_box_3d.gd"):
 		hit_box_3d_added = true
 		add_custom_type("HitBox3D", "Area3D",
-			preload("3d/hit_box_3d/hit_box_3d.gd"),
-			preload("3d/hit_box_3d/hit_box_3d.svg"))
+			load(addon_path % "3d/hit_box_3d/hit_box_3d.gd"),
+			load(addon_path % "3d/hit_box_3d/hit_box_3d.svg"))
 	if FileAccess.file_exists(addon_path % "3d/hit_scan_3d/hit_scan_3d.gd"):
 		hit_scan_3d_added = true
 		add_custom_type("HitScan3D", "RayCast3D",
-			preload("3d/hit_scan_3d/hit_scan_3d.gd"),
-			preload("3d/hit_scan_3d/hit_scan_3d.svg"))
+			load(addon_path % "3d/hit_scan_3d/hit_scan_3d.gd"),
+			load(addon_path % "3d/hit_scan_3d/hit_scan_3d.svg"))
 	if FileAccess.file_exists(addon_path % "3d/hurt_box_3d/hurt_box_3d.gd"):
 		hurt_box_3d_added = true
 		add_custom_type("HurtBox3D", "Area3D",
-			preload("3d/hurt_box_3d/hurt_box_3d.gd"),
-			preload("3d/hurt_box_3d/hurt_box_3d.svg"))
+			load(addon_path % "3d/hurt_box_3d/hurt_box_3d.gd"),
+			load(addon_path % "3d/hurt_box_3d/hurt_box_3d.svg"))
 
 
 func _exit_tree() -> void:
